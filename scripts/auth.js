990491ffc767abda976121a8efa0972132ad90fb
Myke-Max <scartman1992@gmail.com>
2021-07-31 06:33:10 +0100

    log in log out auth status

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,23 +1,61 @@
-// signup
+// listen auth status change 'return null if user logout'
+
+auth.onAuthStateChanged(user => {
+        if (user) {
+            console.log('login', user)
+        } else {
+            console.log('logout')
+        }
+    })
+    // signup
 const signupForm = document.querySelector('#signup-form');
 console.log(signupForm)
 signupForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  
-  // get user info
-  const email = signupForm['signup-email'].value;
-    const password = signupForm['signup-password'].value;
-    console.log(email,password)
+    e.preventDefault();
 
-  // sign up the user
+    // get user info
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+    console.log(email, password)
+
+    // sign up the user
     auth.createUserWithEmailAndPassword(email, password).then(credential => {
-      console.log(credential)
+        // console.log(credential)
         const modal = document.querySelector('#modal-signup')
-        console.log(modal)
+            // console.log(modal)
         M.Modal.getInstance(modal).close()
         signupForm.reset()
-  })
+    })
 });
 
 
+// logout
 
+const logout = document.querySelector('#logout');
+
+logout.addEventListener('click', (e) => {
+    e.preventDefault()
+    auth.signOut()
+});
+
+
+// Login
+
+
+const loginForm = document.querySelector('#login-form');
+
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value
+
+    auth.signInWithEmailAndPassword(email, password)
+
+    // close and clear form
+
+    const modal = document.querySelector('#modal-login')
+    M.Modal.getInstance(modal).close()
+    loginForm.reset()
+
+})
